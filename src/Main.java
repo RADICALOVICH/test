608ca4c6034c3a882b64f07c74de492f73c6ae45
Valeriy Poluboyarov <vp@MacBook-Pro-Valeriy.local>
2025-10-30 21:14:20 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String email = scanner.nextLine();
+        System.out.println(pochtoviYashik(email));
+        int[] shit = new int[5];
+        System.out.println(shit.length);
+    }
+
+
+    public static String pochtoviYashik(String a) {
+        int indexSobaka = a.indexOf('@');
+        String pochtoviYashik = a.substring(indexSobaka + 1);
+        return pochtoviYashik;
+
+    }
+
+}
