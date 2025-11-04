dc4501e3ac56f8aed73983ce7ce2e12a1bb30d62
Valeriy Poluboyarov <vp@MacBook-Pro-Valeriy.local>
2025-11-04 14:22:34 +0300

    New branch commit

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("new branch");
         Scanner scanner = new Scanner(System.in);
         String email = scanner.nextLine();
         System.out.println(pochtoviYashik(email));
