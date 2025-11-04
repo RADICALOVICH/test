eac659647660600a91eadbafca68ec4eff1317d0
Valeriy Poluboyarov <vp@MacBook-Pro-Valeriy.local>
2025-11-04 21:05:09 +0300

    init branch_2

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("main");
+        System.out.println("branch_2");
         Scanner scanner = new Scanner(System.in);
         String email = scanner.nextLine();
         System.out.println(pochtoviYashik(email));
